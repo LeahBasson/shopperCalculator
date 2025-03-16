8681f7170cf4bf69b5d9dab1f1fbbb40ceb38175
LeahBasson <leahbasson54@gmail.com>
2025-03-16 21:13:52 +0200

    :tada:

---
+++ shopperCalculator/src/ShopperCalculator.java
@@ -0,0 +1,62 @@
+
+import java.util.Scanner;
+
+
+public class ShopperCalculator {
+
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        String item1,item2,item3,item4,item5;
+        double price1,price2,price3,price4,price5,sum;
+        
+        System.out.print("Enter the name of the first item you want to buy: ");
+        item1 = scn.next();
+        
+        System.out.print("Enter the price of the first item: R");
+        price1 = scn.nextDouble();
+        
+        System.out.print("Enter the name of the second item you want to buy: ");
+        item2 = scn.next();
+        
+        System.out.print("Enter the price of the second item: R");
+        price2 = scn.nextDouble();
+        
+        System.out.print("Enter the name of the third item you want to buy: ");
+        item3 = scn.next();
+        
+        System.out.print("Enter the price of the third item: R");
+        price3 = scn.nextDouble();
+        
+        System.out.print("Enter the name of the fourth item you want to buy: ");
+        item4 = scn.next();
+        
+        System.out.print("Enter the price of the fourth item: R");
+        price4 = scn.nextDouble();
+        
+        System.out.print("Enter the name of the fifth item you want to buy: ");
+        item5 = scn.next();
+        
+        System.out.print("Enter the price of the fifth item: R");
+        price5 = scn.nextDouble();
+        
+        System.out.println("===================================================");
+        System.out.println("You have selected to buy the following items:");
+        System.out.println("---------------------------------------------------");
+        
+        System.out.println("Item: " + item1 + " at R" + price1);
+        System.out.println("Item: " + item2 + " at R" + price2);
+        System.out.println("Item: " + item3 + " at R" + price3);
+        System.out.println("Item: " + item4 + " at R" + price4);
+        System.out.println("Item: " + item5 + " at R" + price5);
+        
+        System.out.println("---------------------------------------------------");
+        
+        
+        sum = price1 + price2 + price3 + price4 + price5;
+                
+        System.out.println("Total for all items: R" + sum);
+    }
+    
+}
